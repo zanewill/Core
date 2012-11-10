ee301ac63611a47e096c6008f988e09e80fcd814
Krzysztof Kozmic <krzysztof.kozmic@gmail.com>
2012-11-10 10:52:51 +1000

    moving where it belongs

--- src/Castle.Core/DynamicProxy/Contributors/DelegateMembersCollector.cs
+++ src/Castle.Core/DynamicProxy/Contributors/DelegateMembersCollector.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
+// Copyright 2004-2011 Castle Project - http://www.castleproject.org/
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,12 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace Castle.DynamicProxy.Generators
+namespace Castle.DynamicProxy.Contributors
 {
 	using System;
 	using System.Reflection;
 
-	using Castle.DynamicProxy.Contributors;
+	using Castle.DynamicProxy.Generators;
 
 	public class DelegateMembersCollector : MembersCollector
 	{
